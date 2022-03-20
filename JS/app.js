190643e6d614cfa23db668f7d8df04618a2d74a2
Blaise Hala <blaisehala@gmail.com>
2022-03-20 16:35:56 +0300

    bug

---
+++ JS/app.js
@@ -0,0 +1,38 @@
+.box-menu {
+  background-color: white;
+  height:24rem;
+  width: 24rem;
+  border-style: groove;
+  border-radius: 10px;
+} 
+
+
+.image-menu {
+  height: 70%;
+
+  border-radius: 20px;
+  padding: 15px;
+} 
+
+
+.image-menu2 {
+  height: 200px;
+  width: 500px;
+
+} 
+
+
+
+
+.toppings-content {
+  display: none;
+}
+
+.box-box {
+  background-color: white;
+  /* height: 24rem; */
+  /* width: 40rem; */
+  border-style: groove;
+  border-radius: 7px;
+} 
+
