4b1cc31b90956ca4066200c18d28495b2f3e1083
Blaise Hala <blaisehala@gmail.com>
2022-03-21 09:27:24 +0300

    feat:adds javascript functionality

--- JS/app.js
+++ JS/app.js
@@ -13,3 +13,27 @@ $('document').ready(function(){
 
 
 
+function message (){
+  var name = document.getElementById('name');
+  var telephone = document.getElementById('tel');
+  var location= document.getElementById('location');
+  var x = document.getElementById('para');
+
+
+  if (name.value === "" || telephone.value === '') {
+    alert('please fill in relevant information')
+  } else {
+    x.innerText = (`${[name.value]} We have received your Order.Thank you for reaching out to Papa's `)
+  }
+}
+
+
+const form = document.getElementById('sbb');
+
+form.addEventListener('click', function(event){
+
+  event.preventDefault();
+  
+  message();
+  
+  });
